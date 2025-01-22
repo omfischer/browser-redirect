2e62c1cec90206f0694932118e41f6f174472556
Ole-Martin Fischer <ole-martin@jotta.no>
2025-01-22 14:34:18 +0100

    ads

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import OpenInBrowser from './OpenInBrowser'
 
 function App() {
   const isInAppBrowser = () => {
