7fef6a5012d272ebc6ce3057a53f41d68c0427fc
Ole-Martin Fischer <ole-martin@jotta.no>
2025-01-29 14:11:23 +0100

    asødfma

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <head>
-      <meta property="og:image" content="https://example.com/image.jpg" />
-      <meta property="og:title" content="Your Title" />
-      <meta property="og:description" content="Your Description" />
-    </head>
     <App />
   </StrictMode>,
 )
